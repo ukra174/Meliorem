b1e0ad73a1b57421367aad7de0e956bcbd3f6bad
Ivan <77243875+ukra174@users.noreply.github.com>
2024-12-26 20:15:26 +0100

    Sort quests by date(new first)

--- scripts/create_tree.py
+++ scripts/create_tree.py
@@ -11,5 +11,13 @@ for problem in problem_mds:
     meta = json.loads(meta_text)
     meta["categories"].sort()
     problems["problems"].append({problem:meta})
+# Function to convert date string to a tuple (year, month, day) 
+def date_key(problem): 
+    for key, value in problem.items(): 
+        day, month, year = map(int, value["created"].split("/")) 
+        return (year, month, day) # Sorting the problems list by the created date
+    
+problems["problems"] = sorted(problems["problems"], key=date_key,reverse=True)
+
     
 open("problems.json","w").write(json.dumps(problems))
