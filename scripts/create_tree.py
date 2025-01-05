d483731ebb560311521e56cb413d204bbca2f8ab
Ivan Kazakov <vania.kazakov2006@gmail.com>
2025-01-05 18:41:44 +0100

    Add support for group quests

--- scripts/create_tree.py
+++ scripts/create_tree.py
@@ -6,7 +6,12 @@ print(problem_mds)
 
 problems = {"problems":[]}
 for problem in problem_mds:
-    meta_text = open(f"./problems/{problem}","r").read().split("<metadata>")[1].split("</metadata>")[0]
+    full_path = f"./problems/{problem}"
+    if(os.path.isdir(full_path)):
+        meta_text = open(f"{full_path}/metadata.json","r").read()
+        problem+="/0.md"
+    else:
+        meta_text = open(full_path,"r").read().split("<metadata>")[1].split("</metadata>")[0]
     print(meta_text)
     meta = json.loads(meta_text)
     meta["categories"].sort()
@@ -20,4 +25,4 @@ def date_key(problem):
 problems["problems"] = sorted(problems["problems"], key=date_key,reverse=True)
 
     
-open("problems.json","w").write(json.dumps(problems))
+open("problems.json","w").write(json.dumps(problems,indent=4))
